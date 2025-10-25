e9bf0735653815a0acf4ef22575fe96ac1a90321
Kyle Brooks <consist-canteen8e@icloud.com>
2025-10-25 17:50:01 +0200

    feat: move quotes to a separate file for better organization

--- src/components/QuoteCard.tsx
+++ src/components/QuoteCard.tsx
@@ -1,42 +1,6 @@
 import { useMemo } from 'react'
 import { Quote } from 'lucide-react'
-
-const QUOTES = [
-  {
-    text:
-      "No matter how many mistakes you make or how slow you progress, you are still way ahead of everyone who isn't trying.",
-    author: 'Tony Robbins',
-  },
-  {
-    text: 'Expect the best. Prepare for the worst. Capitalize on what comes.',
-    author: 'Zig Ziglar',
-  },
-  {
-    text: "Nothing is impossible, the word itself says ‘I’m possible.’",
-    author: 'Audrey Hepburn',
-  },
-  {
-    text:
-      'For there is always light. If only we’re brave enough to see it. If only we’re brave enough to be it.',
-    author: 'Amanda Gorman',
-  },
-  {
-    text: "Do your thing and don't care if they like it.",
-    author: 'Tina Fey',
-  },
-  {
-    text: 'I’d rather regret the things I’ve done than the things I haven’t done.',
-    author: 'Lucille Ball',
-  },
-  {
-    text: 'Try to be a rainbow in someone else’s cloud.',
-    author: 'Maya Angelou',
-  },
-  {
-    text: 'A dead end is just a good place to turn around.',
-    author: 'Naomi Judd',
-  },
-]
+import { QUOTES } from '../utils/quotes'
 
 export default function QuoteCard() {
   const quote = useMemo(() => QUOTES[Math.floor(Math.random() * QUOTES.length)], [])
