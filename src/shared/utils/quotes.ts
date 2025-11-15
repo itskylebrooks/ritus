7edb679521745ece755f4b5cca416550fc1ac19e
Kyle Brooks <consist-canteen8e@icloud.com>
2025-11-15 13:52:09 +0100

    fix: cut the too long quote

--- src/shared/utils/quotes.ts
+++ src/shared/utils/quotes.ts
@@ -317,7 +317,7 @@ export const QUOTES = [
   },
   {
     text:
-      "Again, you can't connect the dots looking forward; you can only connect them looking backwards. So you have to trust that the dots will somehow connect in your future. You have to trust in something - your gut, destiny, life, karma, whatever. This approach has never let me down, and it has made all the difference in my life.",
+      "Again, you can't connect the dots looking forward; you can only connect them looking backwards. So you have to trust that the dots will somehow connect in your future.",
     author: 'Steve Jobs',
   },
   {
